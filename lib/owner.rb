7d82a671e78d22b71fa3a4cc458355f9d1ccccea
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-06 22:33:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -10,5 +10,15 @@ class Owner
     @@owners << self
   end
   
-  
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
 end
