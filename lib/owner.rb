8eafdecc9f0976ab4577562b9bd9573d3cb86b8f
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-06 22:53:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,22 +1,6 @@
 class Owner
   attr_reader :name 
-
   
   def initialize(name)
-    @species = "human"
     @name = name
-    @@owners << self
   end
-  
-  def self.all
-    @@owners
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-end
