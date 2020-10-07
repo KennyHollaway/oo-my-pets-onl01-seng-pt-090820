0dd160df19c3a769a60e502fd872b04306f6a938
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-07 00:43:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,18 +64,5 @@ class Owner
     end
   end
 
-
-  def list_pets
-    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
-  end
-
   def sell_pets
-    pets.each do |pet, arr|
-      arr.map do |pet|
-        pet.mood = 'nervous'
-      end
-      arr.clear
-    end
-  end
-
-end
+    
